edb8ebd415d111b41831bd8791a42d1356b14e5f
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2023-05-08 00:06:01 +0700

    done signup

---
+++ app/src/main/java/com/example/plantapp/data/User.kt
@@ -0,0 +1,3 @@
+package com.example.plantapp.data
+
+data class User(var email: String, var fullname: String)
--- app/src/main/java/com/example/plantapp/ui/auth/SignUpFragment.kt
+++ app/src/main/java/com/example/plantapp/ui/auth/SignUpFragment.kt
@@ -9,12 +9,14 @@ import android.view.ViewGroup
 import android.widget.Toast
 import androidx.navigation.fragment.findNavController
 import com.example.plantapp.R
+import com.example.plantapp.data.User
 import com.example.plantapp.databinding.FragmentSignUpBinding
 import com.example.plantapp.databinding.FragmentWalkThroughBinding
 import com.example.plantapp.utils.isValidEmail
 import com.example.plantapp.utils.isValidPassword
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
 class SignUpFragment : Fragment() {
@@ -22,9 +24,9 @@ class SignUpFragment : Fragment() {
     private var _binding: FragmentSignUpBinding? = null
     private val binding get() = _binding!!
     private lateinit var auth: FirebaseAuth
+    private val db = Firebase.firestore
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         _binding = FragmentSignUpBinding.inflate(inflater, container, false)
         return binding.root
@@ -49,7 +51,9 @@ class SignUpFragment : Fragment() {
                                 // Sign in success, update UI with the signed-in user's information
                                 Log.d("TAG", "createUserWithEmail:success")
                                 val user = auth.currentUser
-                                findNavController().popBackStack()
+                                db.collection("auth").document(System.currentTimeMillis().toString())
+                                    .set(User(edtEmail.text.toString(), edtFullName.text.toString()))
+                                    .addOnCompleteListener { findNavController().popBackStack() }
                             } else {
                                 // If sign in fails, display a message to the user.
                                 Log.w("TAG", "createUserWithEmail:failure", task.exception)
