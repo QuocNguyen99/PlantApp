cfdd7736b9114914501bb624a95a056f925f7269
QuocNguyen99 <huynhquocnguyen99@gmail.com>
2023-05-12 01:49:32 +0700

    add shadow

--- app/src/main/java/com/example/plantapp/ui/main/home/HomeFragment.kt
+++ app/src/main/java/com/example/plantapp/ui/main/home/HomeFragment.kt
@@ -1,21 +1,37 @@
 package com.example.plantapp.ui.main.home
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.plantapp.R
+import com.example.plantapp.databinding.FragmentHomeBinding
+import com.google.android.material.shape.MaterialShapeDrawable
+import com.google.android.material.shape.ShapeAppearanceModel
 
 class HomeFragment : Fragment() {
 
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
 }
